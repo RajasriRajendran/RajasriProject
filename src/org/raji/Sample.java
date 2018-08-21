667ec79eff582e167e9a00688a549c72d4bb2870
RajasriRajendran <postmail2rajasri@gmail.com>
2018-08-21 11:50:49 +0530

    This is my Project

---
+++ src/org/raji/Sample.java
@@ -0,0 +1,16 @@
+package org.raji;
+
+import java.util.Scanner;
+
+public class Sample {
+public static void main(String[] args) {
+	int a, b;
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter a and b");
+	 a= scan.nextInt();
+	 b = scan.nextInt();
+	 int c = a+b;
+	 System.out.println(c);
+	
+}
+}
